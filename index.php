7bf16534cd307ca3a9a9cd524948e06a9f269f03
Victor-FP <46601009+Victor-FP@users.noreply.github.com>
2021-03-10 04:04:14 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+  session_start();
+
+  require 'database.php';
+
+  if (isset($_SESSION['user_id'])) {
+    $records = $conn->prepare('SELECT num_reloj, Contraseña FROM usuarios WHERE num_reloj = :num_emp');
+    $records->bindParam(':num_emp', $_SESSION['user_id']);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
+
+    $user = null;
+
+    if (count($results) > 0) {
+      $user = $results;
+    }
+  }
+?>
+
 <html>
     <head>
         <meta charset="utf-8">
@@ -10,12 +29,20 @@
     </head>
     <body>
       
+    <?php if(!empty($user)): ?>
+      <br> Welcome. <?= $user['num_reloj']; ?>
+      <br>You are Successfully Logged In
+      <a href="logout.php">
+        Logout
+      </a>
+    <?php else: ?>
+
 
         <h1>Inicia sesión para continuar.</h1>
-        <form action="index.php" method="post">
-            <input type="text" name="num_emp" placeholder="Ingrese su numero de Empleado">
-            <input type="password" name="pass" placeholder="Ingrese su contraseña">
+        <form action="login.php" method="post">
+            
             <input type="submit" value="Entrar">
         </form>
+        <?php endif; ?>    
     </body>
 </html>
