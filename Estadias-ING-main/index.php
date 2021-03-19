99986047412f049c70c512d83bba70348a0efe11
Victor-FP <46601009+Victor-FP@users.noreply.github.com>
2021-03-19 12:50:26 -0600

    modal box y vista de pdf

--- Estadias-ING-main/index.php
+++ Estadias-ING-main/index.php
@@ -4,7 +4,7 @@
   require 'database.php';
 
   if (isset($_SESSION['user_id'])) {
-    $records = $conn->prepare('SELECT num_reloj, Contraseña, Nombre FROM usuarios WHERE num_reloj = :num_emp');
+    $records = $conn->prepare('SELECT num_reloj, Contraseña, Nombre, Apellido_Pa, Apellido_Ma FROM usuarios WHERE num_reloj = :num_emp');
     $records->bindParam(':num_emp', $_SESSION['user_id']);
     $records->execute();
     $results = $records->fetch(PDO::FETCH_ASSOC);
@@ -25,6 +25,9 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/style.css">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
 
         <?php require 'partials/navbar.php'
@@ -33,7 +36,7 @@
     <body>
       
     <?php if(!empty($user)): ?>
-      <br> Welcome. <?= $user['Nombre']; ?>
+      <br> Bienvenido <?= $user['Nombre'],' ', $user['Apellido_Pa'],' ', $user['Apellido_Ma']; ?>
       <br>Estos son los cursos
       <div id="app" class="row">
         <div class="card col-sm-4" v-for="curso of cursos">
@@ -41,7 +44,24 @@
           <div class="card-body">
             <h5 class="card-title">{{curso.nombre}}</h5>
             <p class="card-text">{{curso.desc}}</p>
-            <button value="CursoNombre" class="btn btn-warning" id="myBtn">Ver curso</button>
+
+            <div class="modal fade" id="miModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	            <div class="modal-dialog modal-lg" role="document" >
+		            <div class="modal-content">
+		            	<div class="modal-header">
+				            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+				          	  <span aria-hidden="true">&times;</span>
+			            	</button>
+				           <h4 class="modal-title" id="myModalLabel">Esto es un modal</h4>
+			           </div>
+			            <div class="modal-body">
+                    <iframe src="https://drive.google.com/file/d/1LN8FYbZkU5K5kbXJ_VaUVxXriIujHXwa/preview" width="640" height="480"></iframe>
+		            	</div>
+	            	</div>
+            	</div>
+            </div>
+            
+            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#miModal">Abrir modal</button>
           </div>
         </div>
     </div>
